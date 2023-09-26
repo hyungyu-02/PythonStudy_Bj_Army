d4f6e143ed8bcb9247fa8eb5010793c9df4822f6
hyungyu <yona20020209@gmail.com>
2023-09-26 13:50:07 +0000

    230926

---
+++ pyCodes/1929.py
@@ -0,0 +1,10 @@
+m, n = map(int, input().split())
+
+if m == 1 and n >= 2:
+    m = 2
+for i in range(m, n+1):
+    for num in range(2, int(i**0.5)+1):
+        if i % num == 0:
+            break
+    else:
+        print(i)
