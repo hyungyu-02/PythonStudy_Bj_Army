234a042309323220b13b370ba6406a68985ba2b1
hyungyu <yona20020209@gmail.com>
2023-10-03 10:01:56 +0000

    231003

--- pyCodes/15652.py
+++ pyCodes/15652.py
@@ -1,19 +1,15 @@
 #백트래킹
 n, m = map(int, input().split())
 
-source = list(map(int, input().split()))
-source.sort()
-
 grp = []
 
-def bt():
+def bt(recent):
     if len(grp) == m:
         print(' '.join(map(str, grp)))
         return
-    for i in source:
-        if i not in grp:
-            grp.append(i)
-            bt()
-            grp.pop()
-            
-bt()
+    for i in range(recent, n+1):
+        grp.append(i)
+        bt(i)
+        grp.pop()
+
+bt(1)
