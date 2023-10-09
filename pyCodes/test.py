7a067656971d3f53fbf107334a2f9328ee47261d
hyungyu <yona20020209@gmail.com>
2023-10-09 14:11:18 +0000

    231009 반복문 안에 반복문 넣을때 변수 i 안겹치게 하자^^

--- pyCodes/test.py
+++ pyCodes/test.py
@@ -1,2 +1,22 @@
-source = list(map(int, input().split()))
-print(source)
+import sys
+read = sys.stdin.readline
+sys.setrecursionlimit(10000)
+dR = [0, 0, -1, 1]
+dC = [-1, 1, 0, 0]
+             
+while True:
+    w, h = map(int, read().split())
+    if w == 0 and h == 0:
+        break
+    
+    land = []
+    for _ in range(h):
+        land.append(list(map(int, read().split())))
+    visited = []
+    visited = [[0 for _ in range(w)] for _ in range(h)]
+    
+    for i in range(h):
+        for j in range(w):
+            print(visited[i][j])
+            
+    
