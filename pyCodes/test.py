0dc5758b44c560146bbc31a6d184740c49a05e00
hyungyu <yona20020209@gmail.com>
2023-11-20 13:34:02 +0000

    231120 dijkstra, heapq

--- pyCodes/test.py
+++ pyCodes/test.py
@@ -1,5 +1,10 @@
-arr = [1,2,3,4,5]
-arr2 = [5,6,7,8]
-arr += arr2
-arr = list(set(arr))
-print(arr)
+import heapq
+
+list = []
+heapq.heappush(list, 2)
+heapq.heappush(list, 6)
+heapq.heappush(list, 1)
+heapq.heappush(list, 3)
+heapq.heappush(list, 1)
+for i in range(len(list)):
+    print(heapq.heappop(list))
