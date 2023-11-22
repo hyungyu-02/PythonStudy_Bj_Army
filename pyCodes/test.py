0d5bb4e86d337c40d1e1acf67e5e5767627be0f0
hyungyu <yona20020209@gmail.com>
2023-11-22 13:38:58 +0000

    231122 tree, recursion

--- pyCodes/test.py
+++ pyCodes/test.py
@@ -1,10 +1,2 @@
-import heapq
-
-list = []
-heapq.heappush(list, 2)
-heapq.heappush(list, 6)
-heapq.heappush(list, 1)
-heapq.heappush(list, 3)
-heapq.heappush(list, 1)
-for i in range(len(list)):
-    print(heapq.heappop(list))
+a = int('B') - int('A')
+print(a)
