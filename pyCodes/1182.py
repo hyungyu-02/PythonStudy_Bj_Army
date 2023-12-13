bd9c44ebd64c880e33c76fb5a71a3204456aac3e
hyungyu <yona20020209@gmail.com>
2023-12-13 13:16:16 +0000

    231213 백트래킹

--- pyCodes/1182.py
+++ pyCodes/1182.py
@@ -1,15 +1,15 @@
+#백트래킹
+#itertools 사용
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 ans = 0
-
-size = 1
-def bound(num, lev):
-    global size
+for i in range(1, n+1):
+    temp = combinations(arr, i) #전체수열, 부분수열의 크기
     
-    if lev == bound:
-        if num == s:
+    for seq in temp:
+        if sum(seq) == s:
             ans += 1
-        return
-    
+print(ans)
--- pyCodes/test.py
+++ pyCodes/test.py
@@ -1,4 +1,1 @@
 arr = [1,2,3,4,5]
-arr.pop(0)
-arr.pop(-1)
-print(arr)
